230caa373aa4945b59848ed9eef94e910f6e3c4e
nueamc <128685126+nueamc@users.noreply.github.com>
2023-05-14 04:40:52 +0000

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,8 +10,8 @@ NEZHA_PORT=
 NEZHA_KEY=
 
 # Argo 固定域名隧道的两个参数,这个可以填 Json 内容或 Token 内容，获取方式看 https://github.com/fscarmen2/X-for-Glitch，不需要的话可以留空，删除或在这三行最前面加 # 以注释
-ARGO_AUTH=''
-ARGO_DOMAIN=
+ARGO_AUTH='{"AccountTag":"fd791517d90c609b2aa4952911e4c477","TunnelSecret":"59MHzEE3yP5pdQ8Fpa2BGCJ9M8eu/CgrYE7n/yrVriM=","TunnelID":"1246e07f-37b5-4c94-93ec-67c7e4f1888a"}'
+ARGO_DOMAIN=gtgh.vjhrdyu5grtuhjdgfd.cf
 
 generate_config() {
   cat > config.json << EOF
